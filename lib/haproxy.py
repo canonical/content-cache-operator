7b0849ff11f1b8ba323d04da2279f70c7a6a791c
Haw Loeung <haw.loeung@canonical.com>
2019-03-15 15:00:23 +1100

    Update HAProxy to support multiple sites
    With HAProxy, you can really only have one 'listen' stanza. We need to
    use the 'Host' header to redirect requests to the correct backends.

--- lib/haproxy.py
+++ lib/haproxy.py
@@ -23,33 +23,63 @@ class HAProxyConf:
     def _generate_stanza_name(self, name):
         return name.replace('.', '-')[0:32]
 
+    def _merge_listen_stanzas(self, config):
+        new = {}
+        for site in config.keys():
+            default_port = 80
+            tls_cert_bundle_path = config[site].get('tls-cert-bundle-path')
+            if tls_cert_bundle_path:
+                default_port = 443
+            port = config[site].get('port', default_port)
+            if not new.get(port):
+                new[port] = {}
+            new[port][site] = config[site]
+            new[port][site]['port'] = port
+        return new
+
     def render_stanza_listen(self, config):
         listen_stanza = """
 listen {name}
 {indent}bind 0.0.0.0:{port}{tls}
-{indent}default_backend backend-{backend_name}
-"""
+{backend_config}"""
+
         rendered_output = []
-        for site in config.keys():
-            default_port = 80
+
+        # For listen stanzas, we need to merge them and use 'use_backend' with
+        # the 'Host' header to direct to the correct backends.
+        config = self._merge_listen_stanzas(config)
+        for port in config:
+            backend_config = []
+            tls_cert_bundle_paths = []
+            for site in config[port].keys():
+                site_conf = config[port][site]
+
+                if len(config[port].keys()) == 1:
+                    name = self._generate_stanza_name(site)
+                else:
+                    name = 'combined-{}'.format(port)
+
+                tls_path = site_conf.get('tls-cert-bundle-path')
+                if tls_path:
+                    tls_cert_bundle_paths.append(tls_path)
+
+                backend_name = site_conf.get('backend-name')
+                if not backend_name:
+                    backend_name = site
+                backend_name = self._generate_stanza_name(backend_name)
+                backend_config.append('{indent}use_backend backend-{backend} if {{ hdr(Host) -i {site} }}\n'
+                                      .format(backend=backend_name, site=site, indent=INDENT))
+
             tls_config = ''
+            if len(tls_cert_bundle_paths) > 0:
+                tls_config = ' ssl crt {}'.format(' '.join(tls_cert_bundle_paths))
 
-            tls_cert_bundle_path = config[site].get('tls-cert-bundle-path')
-            if tls_cert_bundle_path:
-                default_port = 443
-                tls_config = ' ssl crt {}'.format(tls_cert_bundle_path)
-
-            port = config[site].get('port', default_port)
-
-            backend_name = config[site].get('backend-name')
-            if not backend_name:
-                backend_name = site
-            backend_name = self._generate_stanza_name(backend_name)
-
-            output = listen_stanza.format(name=self._generate_stanza_name(site), backend_name=backend_name,
+            if len(backend_config) == 1:
+                backend = backend_config[0].split()[1]
+                backend_config = ['{indent}default_backend {backend}\n'.format(backend=backend, indent=INDENT)]
+            output = listen_stanza.format(name=name, backend_config=''.join(backend_config),
                                           port=port, tls=tls_config, indent=INDENT)
             rendered_output.append(output)
-
         return rendered_output
 
     def render_stanza_backend(self, config):
