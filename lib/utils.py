0680433006e9dafb1aacfd7b8355a4b09f958fae
Haw Loeung <haw.loeung@canonical.com>
2019-03-22 15:21:03 +1100

    Move out next_port_pair() and generate_nagios_check_name() to utils

--- lib/utils.py
+++ lib/utils.py
@@ -3,6 +3,46 @@ import hashlib
 import hmac
 
 
+BASE_CACHE_PORT = 6080
+BASE_BACKEND_PORT = 8080
+BACKEND_PORT_LIMIT = 61000  # sysctl net.ipv4.ip_local_port_range
+
+
+class InvalidPortError(Exception):
+    pass
+
+
+def next_port_pair(cache_port, backend_port,
+                   base_cache_port=BASE_CACHE_PORT,
+                   base_backend_port=BASE_BACKEND_PORT,
+                   backend_port_limit=BACKEND_PORT_LIMIT):
+    if cache_port == 0:
+        cache_port = base_cache_port
+    else:
+        cache_port += 1
+
+    if backend_port == 0:
+        backend_port = base_backend_port
+    else:
+        backend_port += 1
+
+    if cache_port < base_cache_port or cache_port >= base_backend_port:
+        raise InvalidPortError('Dynamically allocated cache_port out of range')
+
+    port_limit = base_backend_port + (base_backend_port - base_cache_port)
+    if port_limit >= backend_port_limit:
+        port_limit = backend_port_limit
+
+    if backend_port < base_backend_port or backend_port >= port_limit:
+        raise InvalidPortError('Dynamically allocated backend_port out of range')
+
+    return (cache_port, backend_port)
+
+
+def generate_nagios_check_name(site):
+    return site.replace('.', '_').replace('-', '_')
+
+
 def generate_token(signing_secret, url_path, expiry):
     expires = datetime.datetime.now() + datetime.timedelta(seconds=expiry)
     expiration = int(expires.timestamp())
--- reactive/content_cache.py
+++ reactive/content_cache.py
@@ -11,11 +11,6 @@ from lib import nginx
 from lib import haproxy as HAProxy
 
 
-BASE_CACHE_PORT = 6080
-BASE_BACKEND_PORT = 8080
-BACKEND_PORT_LIMIT = 61000  # sysctl net.ipv4.ip_local_port_range
-
-
 @reactive.hook('upgrade-charm')
 def upgrade_charm():
     status.maintenance('forcing reconfiguration on upgrade-charm')
@@ -174,19 +169,19 @@ def configure_nagios():
             path = '/?token={}'.format(utils.generate_token(signed_url_hmac_key, '/', 315360000))
 
         # Listen / frontend check
-        check_name = 'site_{}_listen'.format(generate_nagios_check_name(site))
+        check_name = 'site_{}_listen'.format(utils.generate_nagios_check_name(site))
         cmd = '/usr/lib/nagios/plugins/check_http -I 127.0.0.1 -H {site} -p {port}{tls} -u {url}{path} -j GET' \
               .format(site=site, port=default_port, url=url, path=path, tls=tls)
         nrpe_setup.add_check(check_name, '{} site listen check'.format(site), cmd)
 
         # Cache layer check
-        check_name = 'site_{}_cache'.format(generate_nagios_check_name(site))
+        check_name = 'site_{}_cache'.format(utils.generate_nagios_check_name(site))
         cmd = '/usr/lib/nagios/plugins/check_http -I 127.0.0.1 -H {site} -p {cache_port} -u {url}{path} -j GET' \
               .format(site=site, cache_port=cache_port, url=url, path=path)
         nrpe_setup.add_check(check_name, '{} cache check'.format(site), cmd)
 
         # Backend proxy layer check
-        check_name = 'site_{}_backend_proxy'.format(generate_nagios_check_name(site))
+        check_name = 'site_{}_backend_proxy'.format(utils.generate_nagios_check_name(site))
         cmd = '/usr/lib/nagios/plugins/check_http -I 127.0.0.1 -H {site} -p {backend_port} -u {url} -j GET' \
               .format(site=site, backend_port=backend_port, url=url)
         nrpe_setup.add_check(check_name, '{} backend proxy check'.format(site), cmd)
@@ -195,47 +190,12 @@ def configure_nagios():
     reactive.set_flag('nagios-nrpe.configured')
 
 
-class InvalidPortError(Exception):
-    pass
-
-
-def next_port_pair(cache_port, backend_port,
-                   base_cache_port=BASE_CACHE_PORT,
-                   base_backend_port=BASE_BACKEND_PORT,
-                   backend_port_limit=BACKEND_PORT_LIMIT):
-    if cache_port == 0:
-        cache_port = base_cache_port
-    else:
-        cache_port += 1
-
-    if backend_port == 0:
-        backend_port = base_backend_port
-    else:
-        backend_port += 1
-
-    if cache_port < base_cache_port or cache_port >= base_backend_port:
-        raise InvalidPortError('Dynamically allocated cache_port out of range')
-
-    port_limit = base_backend_port + (base_backend_port - base_cache_port)
-    if port_limit >= backend_port_limit:
-        port_limit = backend_port_limit
-
-    if backend_port < base_backend_port or backend_port >= port_limit:
-        raise InvalidPortError('Dynamically allocated backend_port out of range')
-
-    return (cache_port, backend_port)
-
-
 def sites_from_config(sites_yaml):
     conf = yaml.safe_load(sites_yaml)
     cache_port = 0
     backend_port = 0
     for site in conf.keys():
-        (cache_port, backend_port) = next_port_pair(cache_port, backend_port)
+        (cache_port, backend_port) = utils.next_port_pair(cache_port, backend_port)
         conf[site]['cache_port'] = cache_port
         conf[site]['backend_port'] = backend_port
     return conf
-
-
-def generate_nagios_check_name(site):
-    return site.replace('.', '_').replace('-', '_')
