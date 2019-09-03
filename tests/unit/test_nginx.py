200ef35fa8154ceb3065a775b592955a8351462b
ballot <benjamin.allot@canonical.com>
2019-09-03 13:55:08 +0200

    Add the Nginx metrics for cache HIT
    A new option `enable_prometheus_metrics` can be set to True to expose nginx metrics
    about cache hits and HTTP requests in prometheus format.
    This metrics are available on port 9145.
    
    Update tests
    
    All the tests have been adapted with the new bits of configuration and
    new tests have been developped for the new feature.

--- tests/unit/test_nginx.py
+++ tests/unit/test_nginx.py
@@ -5,6 +5,8 @@ import tempfile
 import unittest
 import yaml
 
+import jinja2
+
 sys.path.append(os.path.dirname(os.path.dirname(os.path.dirname(os.path.realpath(__file__)))))
 from lib import nginx  # NOQA: E402
 
@@ -21,10 +23,13 @@ class TestLibNginx(unittest.TestCase):
 
     def test_nginx_config_conf_path(self):
         conf_path = '/etc/nginx/conf.d'
+        alternate_conf_path = '/var/lib/snap/nginx/etc'
         ngx_conf = nginx.NginxConf()
         self.assertEqual(ngx_conf.conf_path, conf_path)
         ngx_conf = nginx.NginxConf(None)
         self.assertEqual(ngx_conf.conf_path, conf_path)
+        ngx_conf = nginx.NginxConf(alternate_conf_path)
+        self.assertEqual(ngx_conf.base_path, alternate_conf_path)
 
     def test_nginx_config_sites_path(self):
         sites_path = '/etc/nginx/sites-available'
@@ -42,6 +47,7 @@ class TestLibNginx(unittest.TestCase):
 
         conf = {}
         conf['cache_max_size'] = '1g'
+        conf['enable_prometheus_metrics'] = False
         conf['cache_path'] = '/var/lib/nginx/proxy'
         conf['listen_address'] = '127.0.0.1'
         # From the given YAML-formatted list of sites, check that each individual
@@ -113,3 +119,73 @@ class TestLibNginx(unittest.TestCase):
 
         # Re-run, no change this time.
         self.assertFalse(ngx_conf.sync_sites(['site1.local', 'site2.local']))
+
+    def test_nginx_config_render_with_metrics(self):
+        '''Test rendering with metrics exposed.'''
+        ngx_conf = nginx.NginxConf()
+
+        with open('tests/unit/files/config_test_nginx_metrics_config.txt', 'r', encoding='utf-8') as f:
+            sites = yaml.safe_load(f.read())
+
+        conf = {}
+        conf['cache_max_size'] = '1g'
+        conf['cache_path'] = '/var/lib/nginx/proxy'
+        conf['listen_address'] = '127.0.0.1'
+        conf['enable_prometheus_metrics'] = True
+        for site, site_conf in sites.items():
+            conf['site'] = site_conf.get('site-name') or site
+            conf['listen_port'] = BASE_LISTEN_PORT
+            conf['locations'] = site_conf.get('locations', {})
+
+            for location, loc_conf in conf['locations'].items():
+                if loc_conf.get('backends'):
+                    loc_conf['backend_port'] = BASE_BACKEND_PORT
+
+            output_file = 'tests/unit/files/nginx_config_rendered_test_output-{}.txt'.format(site)
+            with open(output_file, 'r', encoding='utf-8') as f:
+                output = f.read()
+
+            self.assertEqual(ngx_conf.render(conf), output)
+
+    def test_nginx_config_toggle_metrics_site(self):
+        ''' Test the metrics site
+
+        Check that the activation fo the cache metrics activate the dedicated site for exposing prometheus metrics.
+        '''
+        ngx_conf = nginx.NginxConf(self.tmpdir)
+        os.mkdir(os.path.join(self.tmpdir, 'sites-available'))
+        os.mkdir(os.path.join(self.tmpdir, 'sites-enabled'))
+
+        from lib.nginx import METRICS_SITE
+
+        metrics_site_conf = '{0}.conf'.format(METRICS_SITE)
+
+        # with open('tests/unit/files/nginx_config_rendered_test_output-nginx_metrics.txt', 'r', encoding='utf-8') as f:
+        #     nginx_metrics_conf = f.read()
+        script_dir = os.path.join(os.path.dirname(os.path.realpath(__file__)), '..', '..')
+        env = jinja2.Environment(loader=jinja2.FileSystemLoader(script_dir))
+        template = env.get_template('templates/nginx_metrics_cfg.tmpl')
+        content = template.render({
+            'nginx_conf_path': os.path.join(self.tmpdir, 'conf.d'),
+            'port': nginx.METRICS_PORT})
+        nginx_metrics_conf = content
+
+        self.assertTrue(ngx_conf.toggle_metrics_site(enable_prometheus_metrics=True))
+        # Write again with same contents, this time it should return 'False'
+        # as there's no change, thus no need to restart/reload Nginx.
+        self.assertFalse(ngx_conf.toggle_metrics_site(enable_prometheus_metrics=True))
+        self.assertTrue(os.path.exists(os.path.join(self.tmpdir, 'sites-available', metrics_site_conf)))
+
+        # Compare what's been written out matches what's in tests/unit/files.
+        with open(os.path.join(self.tmpdir, 'sites-available', metrics_site_conf), 'r', encoding='utf-8') as f:
+            output = f.read()
+        self.assertEqual(nginx_metrics_conf, output)
+
+        # Chek link existence
+        self.assertTrue(os.path.islink(os.path.join(self.tmpdir, 'sites-enabled', metrics_site_conf)))
+
+        # Remove the site
+        self.assertTrue(ngx_conf.toggle_metrics_site(enable_prometheus_metrics=False))
+        self.assertFalse(ngx_conf.toggle_metrics_site(enable_prometheus_metrics=False))
+        self.assertFalse(os.path.exists(os.path.join(self.tmpdir, 'sites-available', metrics_site_conf)))
+        self.assertFalse(os.path.exists(os.path.join(self.tmpdir, 'sites-enabled', metrics_site_conf)))
