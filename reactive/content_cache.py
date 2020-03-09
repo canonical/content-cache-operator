282d358ccc86dd84b34ef1e71d2c81f3b2c84e9c
Haw Loeung <haw.loeung@canonical.com>
2020-03-10 08:23:51 +1100

    Added unit test for overriding per-site maxconns

--- reactive/content_cache.py
+++ reactive/content_cache.py
@@ -204,7 +204,7 @@ def configure_haproxy():  # NOQA: C901 LP#1825084
         status.blocked('requires list of sites to configure')
         return
 
-    haproxy = HAProxy.HAProxyConf(max_connections=config['max_connections'])
+    haproxy = HAProxy.HAProxyConf(max_connections=config.get('max_connections', 0))
     sites_secrets = secrets_from_config(config.get('sites_secrets'))
     blacklist_ports = [int(x.strip()) for x in config.get('blacklist_ports', '').split(',') if x.strip()]
     sites = sites_from_config(config.get('sites'), sites_secrets, blacklist_ports=blacklist_ports)
