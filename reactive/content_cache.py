c0ad56a4d2661b8ac8962598a837b2bb663a8ee1
Haw Loeung <haw.loeung@canonical.com>
2020-04-28 15:49:40 +1000

    Fixed based on review

--- reactive/content_cache.py
+++ reactive/content_cache.py
@@ -308,7 +308,7 @@ def configure_haproxy():  # NOQA: C901 LP#1825084
     monitoring_password = haproxy.monitoring_password
     if not monitoring_password:
         monitoring_password = host.pwgen(length=20)
-    num_procs = config.get('haproxy_processes', 0)
+    num_procs = config.get('haproxy_processes')
     num_threads = config.get('haproxy_threads')
     tls_cipher_suites = config.get('tls_cipher_suites')
     rendered_config = haproxy.render(new_conf, num_procs, num_threads, monitoring_password, tls_cipher_suites)
