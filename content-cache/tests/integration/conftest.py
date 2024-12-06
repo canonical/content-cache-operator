69f0e4305eac3c97eadd21471cd99848415c7d14
yhaliaw <43424755+yhaliaw@users.noreply.github.com>
2024-12-06 16:16:35 +0800

    Integration test using content-cache-backends-config charm (#10)

--- content-cache/tests/integration/conftest.py
+++ content-cache/tests/integration/conftest.py
@@ -4,6 +4,7 @@
 """Fixture for integration tests."""
 
 
+import asyncio
 import logging
 import secrets
 from typing import AsyncIterator
@@ -12,6 +13,7 @@ import pytest
 import pytest_asyncio
 from juju.application import Application
 from juju.model import Model
+from pytest_operator.plugin import OpsTest
 
 from tests.integration.helpers import CacheTester, deploy_http_app, get_app_ip
 
@@ -47,37 +49,62 @@ def charm_file_fixture(pytestconfig: pytest.Config) -> str:
     return f"./{file}"
 
 
+@pytest_asyncio.fixture(name="config_charm_file", scope="module")
+async def config_charm_file_fixture(ops_test: OpsTest) -> AsyncIterator[str]:
+    """Build the configuration charm file and return the path."""
+    path = await ops_test.build_charm("../content-cache-backends-config")
+    yield str(path)
+
+
 @pytest_asyncio.fixture(name="model", scope="module")
 async def model_fixture(ops_test) -> AsyncIterator[Model]:
     """The juju model for testing."""
     yield ops_test.model
 
 
-@pytest_asyncio.fixture(name="app", scope="module")
-async def app_fixture(model: Model, charm_file: str, app_name: str) -> AsyncIterator[Application]:
-    """The content-cache charm application for testing."""
-    app: Application = await model.deploy(charm_file, app_name, base="ubuntu@24.04")
+@pytest_asyncio.fixture(name="applications", scope="module")
+async def deploy_applications_fixture(
+    model: Model,
+    charm_file: str,
+    config_charm_file: str,
+    app_name: str,
+    config_app_name: str,
+    cert_app_name: str,
+) -> AsyncIterator[dict[str, Application]]:
+    """Deploy all applications in parallel."""
+    app_task = model.deploy(charm_file, app_name, base="ubuntu@24.04")
+    config_app_task = model.deploy(config_charm_file, config_app_name, num_units=0)
+    cert_app_task = model.deploy(
+        CERT_CHARM_NAME, cert_app_name, base="ubuntu@22.04", channel="latest/edge"
+    )
+    app, config_app, cert_app = await asyncio.gather(app_task, config_app_task, cert_app_task)
     await model.wait_for_idle([app.name], status="blocked", timeout=15 * 60)
-    yield app
+    await model.wait_for_idle([cert_app.name], status="active", timeout=15 * 60)
+    yield {app_name: app, config_app_name: config_app, cert_app_name: cert_app}
+
+
+@pytest_asyncio.fixture(name="app", scope="module")
+async def app_fixture(
+    app_name: str, applications: dict[str, Application]
+) -> AsyncIterator[Application]:
+    """The content-cache charm application for testing."""
+    yield applications[app_name]
 
 
 @pytest_asyncio.fixture(name="config_app", scope="module")
-async def config_app_fixture(model: Model, config_app_name: str) -> AsyncIterator[Application]:
+async def config_app_fixture(
+    config_app_name: str, applications: dict[str, Application]
+) -> AsyncIterator[Application]:
     """The configuration charm application for testing."""
-    app: Application = await model.deploy(
-        CONFIG_CHARM_NAME, config_app_name, base="ubuntu@24.04", channel="latest/edge", revision=5
-    )
-    yield app
+    yield applications[config_app_name]
 
 
 @pytest_asyncio.fixture(name="cert_app", scope="module")
-async def cert_app_fixture(model: Model, cert_app_name: str) -> AsyncIterator[Application]:
+async def cert_app_fixture(
+    cert_app_name: str, applications: dict[str, Application]
+) -> AsyncIterator[Application]:
     """The TLS certificate charm application for testing."""
-    app: Application = await model.deploy(
-        CERT_CHARM_NAME, cert_app_name, base="ubuntu@22.04", channel="latest/edge"
-    )
-    await model.wait_for_idle([app.name], status="active", timeout=15 * 60)
-    yield app
+    yield applications[cert_app_name]
 
 
 @pytest.fixture(name="http_ok_path", scope="module")
@@ -107,6 +134,7 @@ async def http_ok_app_fixture(
 
 @pytest_asyncio.fixture(name="http_ok_ip", scope="module")
 async def http_ok_ip_fixture(http_ok_app: Application) -> str:
+    """The IP to the test HTTP application that returns OK."""
     return await get_app_ip(http_ok_app)
 
 
@@ -114,6 +142,7 @@ async def http_ok_ip_fixture(http_ok_app: Application) -> str:
 async def cache_tester_fixture(
     model: Model, app: Application, config_app: Application, cert_app: Application
 ) -> AsyncIterator[CacheTester]:
+    """Get the cache tester."""
     unit = app.units[0]
     tester = CacheTester(model, app, config_app, cert_app)
 
@@ -122,6 +151,6 @@ async def cache_tester_fixture(
     # This removes the integration and configurations.
     await tester.reset()
 
-    await model.wait_for_idle([app.name], status="blocked", timeout=5 * 60)
+    await model.wait_for_idle([app.name], status="blocked", timeout=10 * 60)
     assert unit.workload_status_message == "Waiting for integration with config charm"
     # The configuration charm is removed due to being subordinate charm with no relation.
