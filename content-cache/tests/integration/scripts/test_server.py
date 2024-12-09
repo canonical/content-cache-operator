77ed34f930973b01817decadd5f6d07e495dfae2
Sebastien Georget <seb4stien@georget.name>
2024-12-09 11:48:11 +0100

    Add methods to switch the healthy state

--- content-cache/tests/integration/scripts/test_server.py
+++ content-cache/tests/integration/scripts/test_server.py
@@ -10,7 +10,7 @@ from dataclasses import dataclass
 from datetime import datetime
 from http.server import BaseHTTPRequestHandler, HTTPServer
 from ipaddress import IPv4Address
-from typing import Type
+from typing import Type, cast
 
 # Script for testing only.
 # flake8: noqa
@@ -23,6 +23,12 @@ class HTTPServerArgs:
     path: str
     status_code: int
     message: str
+
+
+class SimpleServer(HTTPServer):
+    def __init__(self, address, handler):
+        self.healthy = True
+        super().__init__(address, handler)
 
 
 def get_args():
@@ -57,23 +63,36 @@ def create_request_handler(
 ) -> Type[BaseHTTPRequestHandler]:
     class RequestHandler(BaseHTTPRequestHandler):
         def do_GET(self):
+            response = ""
+            server = cast(SimpleServer, self.server)
             if self.path == "/health":
+                if server.healthy:
+                    self.send_response(200)
+                else:
+                    self.send_response(500)
+
+            elif self.path == "/turn-healthy":
+                server.healthy = True
                 self.send_response(200)
-                self.end_headers()
-                return
 
-            if self.path != path:
+            elif self.path == "/turn-unhealthy":
+                server.healthy = False
+                self.send_response(200)
+
+            elif self.path == path:
+                response = json.dumps({"message": message, "time": str(datetime.now())})
+                self.send_response(status_code)
+                self.send_header("Content-type", "application/json")
+                self.send_header("Content-Length", str(len(response)))
+                self.send_header("Cache-control", "")
+            else:
                 self.send_response(404)
-                self.end_headers()
-                return
 
-            response = json.dumps({"message": message, "time": str(datetime.now())})
-            self.send_response(status_code)
-            self.send_header("Content-type", "application/json")
-            self.send_header("Content-Length", str(len(response)))
-            self.send_header("Cache-control", "")
             self.end_headers()
-            self.wfile.write(response.encode("utf8"))
+            if response:
+                self.wfile.write(response.encode("utf8"))
+
+            return
 
     return RequestHandler
 
@@ -81,7 +100,7 @@ def create_request_handler(
 def main():
     args = get_args()
     request_handler = create_request_handler(args.path, args.status_code, args.message)
-    HTTPServer((args.ip, args.port), request_handler).serve_forever()
+    SimpleServer((args.ip, args.port), request_handler).serve_forever()
 
 
 if __name__ == "__main__":
