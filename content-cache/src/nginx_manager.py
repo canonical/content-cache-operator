b3ee89bb251a8781e76753d272e0a32842964cce
Sebastien Georget <sebastien.georget@canonical.com>
2025-09-03 17:58:05 +0200

    Fix lint

--- content-cache/src/nginx_manager.py
+++ content-cache/src/nginx_manager.py
@@ -478,12 +478,13 @@ def _get_upstream_healthchecks_worker(upstream: str, config: LocationConfig) -> 
         A string with the lua script for the healthcheck workers.
     """
     valid_status_str = ",".join(str(status) for status in config.healthcheck_config.valid_status)
+    hc_path = config.healthcheck_config.path
     return rf"""ok, err = hc.spawn_checker{{
             shm = "healthcheck",
             upstream = "{upstream}",
             type = "{config.protocol.value}",
 
-            http_req = "GET {config.healthcheck_config.path} HTTP/1.0\r\nHost: {config.hostname}\r\n\r\n",
+            http_req = "GET {hc_path} HTTP/1.0\r\nHost: {config.hostname}\r\n\r\n",
 
             port = {getservbyname(config.protocol.value)},
             interval = {config.healthcheck_config.interval},
@@ -499,7 +500,7 @@ def _get_upstream_healthchecks_worker(upstream: str, config: LocationConfig) -> 
             ngx.log(ngx.ERR, "failed to spawn health checker: ", err)
             return
         end
-    """  # noqa: line-too-long
+    """
 
 
 def _get_location_config_keys(
